1545cc7d02fb916b809eb83535e7a727962c63dd
Francisco Chaves <francisco.jschaves@gmail.com>
2019-10-29 10:43:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.createServer(function(request, response) {
+	
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    response.write('Hello world');
+		   
+    response.end();
+	
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000');
